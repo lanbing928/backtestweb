14655e3f60a5992bdf937ddeeef40011cd222c52
lee <libing531@126.com>
2016-07-01 19:19:14 +0800

    优化判断用户是否登录机制

--- common/CheckUserLogin.class.php
+++ common/CheckUserLogin.class.php
@@ -13,31 +13,36 @@ class CheckLogin
     {
         $userCookie = new Cookies();
         $userInfo = $userCookie->get(iwookongConfig::$usercookie);
+        $user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : "";
+        $token = isset($_SESSION['token']) ? $_SESSION['token'] : "";
+        if (!empty($user_id) && !empty($token)) {
+            return 1;
+        } else {
+            if (isset($userInfo) && $userInfo != null) {
+                $info = json_decode($userInfo, true);
+                $userName = $info['user_name'];
+                $user_id = $info['user_id'];
+                $token = $info['token'];
+                if (!empty($userName) && $user_id > 0 && !empty($token)) {
+                    $_SESSION['user_id'] = $user_id;
+                    $_SESSION['token'] = $token;
+                    $userCookie->set(iwookongConfig::$usercookie, json_encode(array(
+                        "user_id" => $user_id,
+                        "user_name" => $userName,
+                        "token" => $token
+                    )), 864000);
+                }
+                $user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : "";
+                $token = isset($_SESSION['token']) ? $_SESSION['token'] : "";
 
-        if (isset($userInfo) && $userInfo != null) {
-            $info = json_decode($userInfo, true);
-            $userName = $info['user_name'];
-            $user_id = $info['user_id'];
-            $token = $info['token'];
-            if (!empty($userName) && !empty($userid) && !empty($token)) {
-                $_SESSION['user_id'] = $user_id;
-                $_SESSION['token'] = $token;
-                $userCookie->set(iwookongConfig::$usercookie, json_encode(array(
-                    "user_id" => $user_id,
-                    "user_name" => $userName,
-                    "token" => $token
-                )), 864000);
-            }
-            $user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : "";
-            $token = isset($_SESSION['token']) ? $_SESSION['token'] : "";
-
-            if (!empty($user_id) && !empty($token)) {
-                return 1;
+                if (!empty($user_id) && !empty($token)) {
+                    return 1;
+                } else {
+                    return -1;
+                }
             } else {
                 return -1;
             }
-        } else {
-            return -1;
         }
     }
 }
