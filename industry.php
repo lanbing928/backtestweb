d2a53acd541d60cd1d13bda696bfa3da4b2f2db5
lee <libing531@126.com>
2016-07-04 23:17:46 +0800

    修复点击新闻切换标签时，热力图会同步刷新的bug

--- industry.php
+++ industry.php
@@ -98,7 +98,7 @@ if (empty($industryName)) {
         <div class="wk-con-industry">
             <p class="wk-hot-title"><?php echo $industryName ?>行业热度情况</p>
             <div class="wk-con-box">
-                <ul class="nav nav-tabs" role="tablist">
+                <ul class="nav nav-tabs wk-hotmap" role="tablist">
                     <li role="presentation" class="active"><a href="#industry-view" aria-controls="industry-view" role="tab" data-toggle="tab">查看热度</a></li>
                     <li role="presentation"><a href="#industry-search" aria-controls="industry-search" role="tab" data-toggle="tab">搜索热度</a></li>
                     <li role="presentation"><a href="#industry-follow" aria-controls="industry-follow" role="tab" data-toggle="tab">关注热度</a></li>
