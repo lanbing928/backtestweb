3ac988f7194105440936fafca2e0bf989a2b234f
abing <libing531@126.com>
2016-07-15 12:57:38 +0800

    增加获取事件关联信心/达人观点/快讯/公告接口

---
+++ ajax/ajax_get_event_relatedinfo.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * 获取事件新闻
+ */
+require_once(dirname(__FILE__) . "/../common/Request.class.php");
+require_once(dirname(__FILE__) . "/../common/iwookongConfig.class.php");
+require_once(dirname(__FILE__) . "/../common/CheckUserLogin.class.php");
+if (CheckLogin::check() == -1) {
+    print_r(json_encode(array("status" => -1, "result" => "未知登录状态")));
+    return;
+}
+$info_type = isset($_POST['info_type']) ? $_POST['info_type'] : "";
+$key_name = isset($_POST['key_name']) ? $_POST['key_name'] : "";
+$url = iwookongConfig::$requireUrl . "eventinfo/1/event_relatedinfo.fcgi";
+$result = RequestUtil::get($url, 
+array(
+    'user_id' => $_SESSION['user_id'],
+    'token'=> $_SESSION["token"],
+    'info_type'=>$info_type,//0 - 新闻 1-快讯 2-自媒体 4-公告
+    'key_name'=>$key_name
+    )
+);
+$jsonresult = json_decode($result, true);
+if ($jsonresult['status'] != "0") {
+    print_r($result);
+    return;
+} else {
+    if ($jsonresult['msg'] == "权限不够") {
+        print_r(json_encode(array("status" => -100, "result" => $jsonresult['msg'])));
+    } else {
+        print_r(json_encode(array("status" => 0, "result" => $jsonresult['msg'])));
+        return;
+    }
+}
