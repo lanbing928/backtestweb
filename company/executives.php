729b4b94505373456093edd91b923886bb4a3416
lidanni <lidanni@kunyan-inc.com>
2016-07-26 18:36:12 +0800

    修改公司高管下拉框引入sweetalert

--- company/executives.php
+++ company/executives.php
@@ -18,6 +18,7 @@ if (CheckLogin::check() == -1) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" href="http://cdn.bootcss.com/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://cdn.bootcss.com/sweetalert/1.1.3/sweetalert.min.css">
     <link rel="stylesheet" href="../static/plugins/typeahead/jquery.typeahead.min.css">
     <link rel="stylesheet" href="../static/css/common.min.css">
     <link rel="stylesheet" href="../static/css/index.min.css">
@@ -26,26 +27,27 @@ if (CheckLogin::check() == -1) {
 <div class="container wk-container wk-company">
     <section class="wk-top-title">
         <label class="wk-topshow-icon"></label>
-        <label class="wk-toshow-name">东旭光电(000413)</label>
-        <!--                <div class="btn-group" role="group">-->
-        <!--                    <i class="fa fa-list-ul" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>-->
-        <!--                    <ul class="dropdown-menu">-->
-        <!--                        <li><a href="#">公司概况</a></li>-->
-        <!--                        <li><a href="#">公司高管</a></li>-->
-        <!--                        <li><a href="#">股本结构</a></li>-->
-        <!--                        <li><a href="#">主要股东</a></li>-->
-        <!--                    </ul>-->
-        <!--                </div>-->
-        <label class="wk-topshow-price wk-down">¥0.000</label>
-        <label class="wk-topshow-price-per">-14.17(0.00%)</label>
+        <label class="wk-toshow-name"></label>
+        <div class="btn-group" role="group">
+            <i class="fa fa-list-ul" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
+            <ul class="dropdown-menu">
+                <li data="1"><a href="#">公司概况</a></li>
+                <li data="2"><a href="#">公司高管</a></li>
+                <li data="3"><a href="#">股本结构</a></li>
+                <li data="4"><a href="#">主要股东</a></li>
+            </ul>
+        </div>
+        <label class="wk-topshow-price"></label>
+        <label class="wk-topshow-price-per"></label>
         <div class="wk-topshow-right">
-            <span class="wk-topshow-dp">沪深：<span class="wk-up">交易中</span><span>
+            <span class="wk-topshow-dp">沪深：<span class="wk-up wk-topshow-status"></span><span>
             <div class="btn-group" style="float: right;">
                 <button type="button" class="btn dropdown-toggle wk-btn-follow" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">+ 关注</button>
                 <ul class="dropdown-menu">
                     <li class="wk-follow-stock" data-follow-name="我的自选股"><a href="#">我的自选股</a></li>
                     <li class="wk-follow-stock" data-follow-name="组合A"><a href="#">组合A</a></li></ul>
             </div>
+        </div>
     </section>
 
     <section class="wk-all-hot executives">
@@ -151,9 +153,10 @@ if (CheckLogin::check() == -1) {
 <script src="http://cdn.bootcss.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script src="http://cdn.bootcss.com/echarts/3.1.10/echarts.min.js"></script>
 <script src="http://cdn.bootcss.com/malihu-custom-scrollbar-plugin/3.1.3/jquery.mCustomScrollbar.concat.min.js"></script>
-<script src="../static/plugins/typeahead/jquery.typeahead.min.js"></script>
+<script src="http://cdn.bootcss.com/sweetalert/1.1.3/sweetalert.min.js"></script>
 <script src="../static/js/all.min.js"></script>
 <script src="../static/js/common.min.js"></script>
 <script src="../static/js/Utility.min.js"></script>
+<script  src="../static/js/page/company.min.js"></script>
 </body>
 </html>
