2b49565ae494e481cf052f9d013eed1fc0414693
lidanni <lidanni@kunyan-inc.com>
2016-08-02 11:47:55 +0800

    调用公司概况接口

---
+++ ajax/company/ajax_get_company.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * 获取公司简介数据
+ */
+require_once(dirname(__FILE__) . "/../../common/Request.class.php");
+require_once(dirname(__FILE__) . "/../../common/iwookongConfig.class.php");
+require_once(dirname(__FILE__) . "/../../common/CheckUserLogin.class.php");
+if (CheckLogin::check() == -1) {
+    print_r(json_encode(array("status" => -1, "result" => "未知登录状态")));
+    return;
+}
+$stockCode = isset($_POST['stockcode']) ? $_POST['stockcode'] : "";
+$stockCode=$stockCode.'s';
+$htmltype = ($_POST['htmltype']);
+if($htmltype=="profile"){
+    $url = iwookongConfig::$requireCompanyUrl . "hot/1/company_profile.fcgi";
+}elseif($htmltype=="cap_stru"){
+    $url = iwookongConfig::$requireCompanyUrl . "hot/1/capital_structure.fcgi";
+}elseif($htmltype=="executives"){
+    $url = iwookongConfig::$requireCompanyUrl . "hot/1/company_administer.fcgi";
+}elseif($htmltype=="stockholder"){
+    $url = iwookongConfig::$requireCompanyUrl . "information/1/stock_base.fcgi";
+}
+$result = RequestUtil::get($url, array(
+    "user_id" => $_SESSION['user_id'],
+    "token" => $_SESSION["token"],
+    "stock_code" =>$stockCode
+));
+
+$jsonresult = json_decode($result, true);
+
+if ($jsonresult['status'] != "0") {
+    print_r($result);
+    return;
+} else {
+    if ($jsonresult['flag'] == -1302 || $jsonresult['flag'] == -1301) {
+        print_r(json_encode(array("status" => -100, "result" => $jsonresult['msg'])));
+        return;
+    }
+}
