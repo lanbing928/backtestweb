a179279e9f68b516ee7c01eb8e0e2e3573c86f34
lidanni <lidanni@kunyan-inc.com>
2016-08-29 15:14:18 +0800

    curl请求

--- common/Request.class.php
+++ common/Request.class.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Created by Lee.
  * Date: 2016/3/30 0030 13:49
@@ -32,7 +31,6 @@ class RequestUtil
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36');
         curl_setopt($ch, CURLOPT_ENCODING, "");
-
         //设置cookie
         if (isset($cookie)) curl_setopt($ch, CURLOPT_COOKIE, $cookie);
         //设置cookie请求文件
@@ -48,7 +46,6 @@ class RequestUtil
         curl_close($ch);
         return $resp;
     }
-
     /**
      * HTTP POST 请求
      * @param string $url 请求地址
@@ -88,5 +85,33 @@ class RequestUtil
         $resp = curl_exec($ch);
         curl_close($ch);
         return $resp;
+    }
+    /**
+     * 模拟post进行url请求
+     * @param string $url
+     * @param array $post_data
+     * @return mixed
+     */
+    public static function request_post($url = '', $post_data = array())
+    {
+        if (empty($url) || empty($post_data)) {
+            return false;
+        }
+        $o = "";
+        foreach ($post_data as $k => $v) {
+            $o .= "$k=" . urlencode($v) . "&";
+        }
+        $post_data = substr($o, 0, -1);
+        $postUrl = $url;
+        $curlPost = $post_data;
+        $ch = curl_init();//初始化curl
+        curl_setopt($ch, CURLOPT_URL, $postUrl);//抓取指定网页
+        curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
+        curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
+        $data = curl_exec($ch);//运行curl
+        curl_close($ch);
+        return $data;
     }
 }
