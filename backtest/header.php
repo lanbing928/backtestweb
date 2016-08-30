078123631cc01c5802a52528e0f2b2a6c593592b
lidanni <lidanni@kunyan-inc.com>
2016-08-30 21:17:50 +0800

    回测平台公共头部页面

---
+++ backtest/header.php
@@ -0,0 +1,15 @@
+<div class="wk-header">
+    <div class="title container">
+        <a href="" class="fl"><div class="wk-backtest title_button">悟空回测</div></a>
+        <a href="" class="fl"><div class="wk-site title_button">返回悟空官网</div></a>
+        <div class="clear"></div>
+    </div>
+    <img src="/static/imgs/backtest/name.png">
+    <div class="typeahead__container">
+        <div class="typeahead__field fl">
+            <span class="typeahead__query"> <input class="wk-head-search" type="search" placeholder="请输入回测关键词，用+号隔开....." autocomplete="off"></span>
+        </div>
+        <input type="button" class="click_back fl" value="回测一下">
+        <div class="clear"></div>
+    </div>
+</div>
