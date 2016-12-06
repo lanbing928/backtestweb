a19f6a2c5677b8f35d8e97cd40b7ecb6eb7faa7a
lidanni <lidanni@kunyan-inc.com>
2016-12-06 14:21:18 +0800

    header头部增加无缝跳转悟空

--- backtest/header.php
+++ backtest/header.php
@@ -3,16 +3,21 @@ require_once(dirname(__FILE__) . "/../common/Cookies.class.php");
 require_once(dirname(__FILE__) . "/../common/iwookongConfig.class.php");
 $userCookie = new Cookies();
 $userInfo = $userCookie->get(iwookongConfig::$usercookie);
-$message=$userCookie->get('message');
+//$message=$userCookie->get('message');
 if (isset($userInfo)) {
     $info = json_decode($userInfo, true);
     $userName = $info['user_name'];
+    $uid = $info['user_id'];
+    $token = $info['token'];
+}else{
+    $uid = '';
+    $token = '';
 }
 ?>
 <div class="wk-header">
     <div class="title container">
         <a href="/" class="fl"><div class="wk-backtest title_button">悟空回测</div></a>
-        <a href="http://stock.iwookong.com" class="fl"><div class="wk-site title_button">返回悟空官网</div></a>
+        <a href="http://t.stock.iwookong.com/ajax/login/nologin.php?uid=<?php echo $uid ?>&token=<?php echo $token ?>" class="fl" target="_blank"><div class="wk-site title_button">返回悟空官网</div></a>
         <div class="clear"></div>
     </div>
     <img src="/static/imgs/backtest/name.png">
